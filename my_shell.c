56c51fa967fe455520a6389aad848fd1043a5042
PKgothatso <ksp331@outlook.com>
2023-12-13 09:05:34 -0800

    Fixed errors

--- my_shell.c
+++ my_shell.c
@@ -1,8 +1,24 @@
 #include "shell.h"
 
-void (check_for_builtins(vars_t *vars))(vars_t *);
-void process_input(vars_t *vars);
-void clean_up(vars_t *vars);
+/* global variable for ^C handling */
+unsigned int sig_flag;
+
+/**
+ * sig_handler - Handles ^C signal interupt.
+ * @uuv: Unused variable (required for signal function prototype).
+ *
+ * Return: Void.
+ */
+void sig_handler(int uuv)
+{
+	(void) uuv;
+
+	if (sig_flag == 0)
+		_puts("\n$ ");
+	else
+		_puts("\n");
+}
+
 
 /**
  * main - Main function for the shell.
@@ -15,74 +31,42 @@ void clean_up(vars_t *vars);
 int main(int argc __attribute__((unused)), char **argv, char **environment)
 {
 	size_t len_buffer = 0;
-	unsigned int is_pipe = 0;
-	vars_t vars = {NULL, NULL, NULL, 0, NULL, 0, NULL};
+
+	unsigned int is_pipe = 0, i;
+	vars_t vars = {NULL, NULL, NULL, 0, NULL, 0, 0};
+
 
 	vars.argv = argv;
 	vars.env = make_env(environment);
 	signal(SIGINT, sig_handler);
-
 	if (!isatty(STDIN_FILENO))
 		is_pipe = 1;
-
 	if (is_pipe == 0)
 		_puts("$ ");
-
 	sig_flag = 0;
-
 	while (getline(&(vars.buffer), &len_buffer, stdin) != -1)
 	{
-		process_input(&vars);
-		clean_up(&vars);
+		sig_flag = 1;
+		vars.count++;
+		vars.commands = tokenize(vars.buffer, ";");
+		for (i = 0; vars.commands && vars.commands[i] != NULL; i++)
+		{
+			vars.av = tokenize(vars.commands[i], "\n \t\r");
+			if (vars.av && vars.av[0])
+				if (check_for_builtins(&vars) == NULL)
+					check_for_path(&vars);
+			free(vars.av);
+		}
+		free(vars.buffer);
+		free(vars.commands);
+		sig_flag = 0;
+		if (is_pipe == 0)
+			_puts("$ ");
+		vars.buffer = NULL;
 	}
-
 	if (is_pipe == 0)
 		_puts("\n");
-
 	free_env(vars.env);
 	free(vars.buffer);
 	exit(vars.status);
-}
-
-/**
- * process_input - Process user input.
- * @vars: Pointer to vars_t struct.
- */
-void process_input(vars_t *vars)
-{
-	unsigned int i;
-
-	sig_flag = 1;
-	vars->count++;
-	vars->commands = tokenize(vars->buffer, ";");
-
-	for (i = 0; vars->commands && vars->commands[i] != NULL; i++)
-	{
-		vars->av = tokenize(vars->commands[i], "\n \t\r");
-
-		if (vars->av && vars->av[0])
-		{
-			void (*builtin_func)(vars_t *) = check_for_builtins(vars);
-			if (builtin_func == NULL)
-
-				check_for_path(vars);
-		}
-
-		free(vars->av);
-	}
-}
-
-/**
- * clean_up - Clean up resources.
- * @vars: Pointer to vars_t struct.
- */
-void clean_up(vars_t *vars)
-{
-	sig_flag = 0;
-	if (is_pipe == 0)
-		_puts("$ ");
-
-	free(vars->buffer);
-	free(vars->commands);
-	vars->buffer = NULL;
 }
